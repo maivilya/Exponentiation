73cd6534cf230be390cd283bdddd666d17eea72e
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2023-02-16 01:38:24 +0500

    exponent method

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,28 @@ public class Main {
     public static void main(String[] args) {
     }
 
+    private static double exponent(int A, int B){
+        double result = 1;
+
+        if (B >= 1){
+            for (int i = 0; i < B; i++){
+                result *= A;
+            }
+        } else if (B <= -1){
+            B *= -1;
+            double tempResult = 1;
+            for (int i = 0; i < B; i++){
+                tempResult *= A;
+            }
+            result = 1/tempResult;
+        } else if (B == 0) {
+            result = 1;
+        } else if (A == 0 && B==0){
+            result = 0;
+        }
+        return result;
+    }
+
     private static int getNumberTerminal(){
         return scan.nextInt();
     }
