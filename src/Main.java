5b19efba93cf729f8488f887dff4d72e37200877
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2023-02-16 02:25:50 +0500

    fixed bugs in exponent method

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,40 @@ import java.util.Scanner;
 
 public class Main {
     private final static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
 
     }
 
-    private static int convertDoubleToInt(double d){
-        return (int)d;
+
+
+    private static int convertDoubleToInt(double d) {
+        return (int) d;
     }
 
-    private static double exponent(int A, int B){
+    private static double exponent(int A, int B) {
         double result = 1;
 
-        if (B >= 1){
-            for (int i = 0; i < B; i++){
+        if (B >= 1) {
+            for (int i = 0; i < B; i++) {
                 result *= A;
             }
-        } else if (B <= -1){
+        } else if (B <= -1) {
             B *= -1;
             double tempResult = 1;
-            for (int i = 0; i < B; i++){
+            for (int i = 0; i < B; i++) {
                 tempResult *= A;
             }
-            result = 1/tempResult;
-        } else if (B == 0) {
+            result = 1 / tempResult;
+        } else if (A !=0 && B == 0) {
             result = 1;
-        } else if (A == 0 && B==0){
+        } else if (A == 0 && B == 0) {
             result = 0;
         }
         return result;
     }
 
-    private static int getNumberTerminal(){
+    private static int getNumberTerminal() {
         return scan.nextInt();
     }
 }
